c7802f61be13d0a1582bdb7a06e984241987caff
jsm041046 <jsm041046@gmail.com>
2022-05-09 21:13:02 +0900

    나머지 순회함수와 노드삭제, 탐색함수 구현

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -136,12 +136,22 @@ void inorderTraversal(Node* ptr)
 
 void preorderTraversal(Node* ptr)
 {
-
+    if(ptr != NULL) //ptr포인터가 값을 가르키고 있을 경우
+    {
+        printf(" [%d] ", ptr->key); //ptr을 먼저 순회해서 출력
+        inorderTraversal(ptr->left); //ptr의 왼쪽을 순회
+        inorderTraversal(ptr->right); //ptr의 오른쪽을 순회
+    }
 }
 
 void postorderTraversal(Node* ptr)
 {
-
+    if(ptr != NULL) //ptr포인터가 값을 가르키고 있을 경우
+    {
+        inorderTraversal(ptr->left); //ptr의 왼쪽을 순회
+        inorderTraversal(ptr->right); //ptr의 오른쪽을 순회
+        printf(" [%d] ", ptr->key); //ptr을 나중에 순회해서 출력
+    }
 }
 
 
@@ -149,7 +159,7 @@ int insert(Node* head, int key)
 {
     Node* temp = (Node*)malloc(sizeof(Node));
     temp->left = NULL;
-    temp->right = NULL;
+    temp->right = NULL; //temp의 좌우노드 초기화
     temp->key = key; //새로 대입할 노드 메모리 할당 및 원소값 대입
 
     Node* p = head->left; //대입할 위치를 탐색하는 포인터
@@ -172,7 +182,7 @@ int insert(Node* head, int key)
     {
         if(prev->key > key) //대입할 노드의 부모 원소값보다 대입하려는 원소값이 작을 경우
             prev->left = temp; //부모 노드의 왼쪽에 대입할 노드를 대입
-        else //대입할 노드의 부모 원소값보다 대입하려는 원소값이 클을 경우
+        else //대입할 노드의 부모 원소값보다 대입하려는 원소값이 클 경우
             prev->right = temp; //부모 노드의 오른쪽에 대입할 노드를 대입
     } else //없을 경우
         head->left = temp; //루트 노드를 대입할 노드로 설정
@@ -182,12 +192,55 @@ int insert(Node* head, int key)
 
 int deleteLeafNode(Node* head, int key)
 {
+    Node* p = head->left; //검색할 노드의 포인터
+    Node* prev = NULL; //검색할 노드의 부모노드 포인터
+
+    while(p != NULL && p->key != key) //p가 끝을 가르키거나, 해당하는 원소값을 찾았을 경우 반복문에서 빠져나감
+    {
+        prev = p;
+        if(p->key > key) //p가 가르키는 값이 찾는 값보다 클 경우
+            p = p->left; //p를 왼쪽 자식으로 이동시킴
+        else //p가 가르키는 값이 찾는 값보다 작을 경우(같은 경우는 해당 while문의 성질 상 애초에 존재할 수 없음)
+            p = p->right; //p를 오른쪽 자식으로 이동시킴
+    }
+
+    if(p == NULL) //찾는 원소가 없을 경우
+    {
+        printf("key is already exist\n");
+        return 0; //비정상 종료
+    }
+    if(p->left != NULL || p->right != NULL) //p가 리프노드가 아닐 경우
+    {
+        printf("that node is not leaf node\n");
+        return 0; //비정상 종료
+    }
+    free(p); //해당 노드를 지움
+    
+    if(prev == NULL) //해당 노드의 부모가 없을 경우
+        head->left = NULL; //루트 노드를 지움
+    else { //부모가 있을 경우
+        if(p == prev->left) //p가 prev의 왼쪽 자식이었을 경우
+            prev->left = NULL; //부모노드의 왼쪽 링크를 끊음
+        else //아닐 경우
+            prev->right = NULL; //부모노드의 오른쪽 링크를 끊음
+    }
 
 }
 
 Node* searchRecursive(Node* ptr, int key)
 {
-
+    if(ptr != NULL) //가르키는 노드가 유효할 경우
+    {
+        if(ptr->key > key) //찾는 원소값이 가르키는 원소값보다 작을 경우
+            return searchRecursive(ptr->left,key); //왼쪽 자식 노드를 탐색하는 함수 재호출
+        else if(ptr->key < key) //찾는 원소값이 가르키는 원소값보다 클 경우
+            return searchRecursive(ptr->right,key); //오른쪽 자식 노드를 탐색하는 함수 재호출
+        else //찾는 값과 가르키는 값이 같을 경우
+            return ptr; //해당 노드의 주소를 반환
+    } else { //가르키는 노드가 유효하지 않을 경우
+        return NULL; //NULL반환
+    }
+    
 }
 
 Node* searchIterative(Node* head, int key)
